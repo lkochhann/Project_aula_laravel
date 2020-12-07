af7ad65f79cbdb0a4c33fa81144addc1384aa0ae
lkochhann <klucypaola@gmail.com>
2020-12-07 18:17:58 -0300

    .

---
+++ app/Http/Models/Client.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Client extends Model
+{
+    //use HasFactory;
+
+    protected $table = 'cliente';
+    public $timestamps = false;
+}
---
+++ resources/views/client.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cliente</title>
+</head>
+<body>
+    <h1>Clientes</h1>         
+    <p>NOME: {{$nome}}</p>
+    <p>EMAIL: {{$email}}</p> 
+</body>
+</html>
